ce5918f621f19602a37d8c96d5722fc3f2f4d929
None <@none@none.com>
2022-06-20 16:42:31 +0300

    broken cpu atomics

--- concurrency/broken-cpu/src/main.rs
+++ concurrency/broken-cpu/src/main.rs
@@ -1,44 +1,86 @@
 use std::thread;
-use std::sync::atomic::{compiler_fence, Ordering};
+use std::sync::atomic::{compiler_fence, Ordering, AtomicI32};
 
-static mut X: i32 = 0;
-static mut Y: i32 = 0;
-static mut R1: i32 = 0;
-static mut R2: i32 = 0;
+fn main() {}
 
-fn main() {
-    for i in 0.. {
+#[test]
+fn broken_cpu() {
+    static mut X1: i32 = 0;
+    static mut X2: i32 = 0;
+    for i in 0.. {    
         unsafe {
-            X = 0;
-            Y = 0;
-            R1 = 0;
-            R2 = 0;
-        }
+            X1 = 0;
+            X2 = 0;
+        }    
         let thread1 = thread::spawn(|| 
             {
                 unsafe {
-                    X = 1;
+                    X1 = 1;
                     compiler_fence(Ordering::SeqCst);
-                    R1 = Y;
+                    X2
                 }
             }
         );
         let thread2 = thread::spawn(|| 
             {
                 unsafe {
-                    Y = 1;
+                    X2 = 1;
                     compiler_fence(Ordering::SeqCst);
-                    R2 = X;
+                    X1
                 }
             }
         );
-        thread1.join().unwrap();
-        thread2.join().unwrap();
-        unsafe {
-            if R1 == 0 && R2 == 0 {
-                println!("Broken CPU iteration {}", i);
-                break;
+        assert!(thread1.join().unwrap() != 0 || thread2.join().unwrap() != 0, "Broken CPU iteration {}", i);
+    }
+}
+
+
+#[test]
+fn atomic_release_acquire() {
+    static X1: AtomicI32 = AtomicI32::new(0);
+    static X2: AtomicI32 = AtomicI32::new(0);
+    for i in 0.. {
+        X1.store(0, Ordering::SeqCst);
+        X2.store(0, Ordering::SeqCst);
+        let thread1 = thread::spawn(
+            || 
+            {
+                X1.store(1, Ordering::Release);
+                X2.load(Ordering::Acquire)
             }
-        }
+        );
+        let thread2 = thread::spawn(
+            || 
+            {
+                X2.store(1, Ordering::Release);
+                X1.load(Ordering::Acquire)
+            }
+        );
+        assert!(thread1.join().unwrap() != 0 || thread2.join().unwrap() != 0, "Broken CPU iteration {}", i);
+    }
+}
+
+#[test]
+fn atomic_sec_cst() {
+    static X1: AtomicI32 = AtomicI32::new(0);
+    static X2: AtomicI32 = AtomicI32::new(0);
+    for i in 0..1000000 {
+        X1.store(0, Ordering::SeqCst);
+        X2.store(0, Ordering::SeqCst);
+        let thread1 = thread::spawn(
+            || 
+            {
+                X1.store(1, Ordering::SeqCst);
+                X2.load(Ordering::SeqCst)
+            }
+        );
+        let thread2 = thread::spawn(
+            || 
+            {
+                X2.store(1, Ordering::SeqCst);
+                X1.load(Ordering::SeqCst)
+            }
+        );
+        assert!(thread1.join().unwrap() != 0 || thread2.join().unwrap() != 0);
     }
 }
