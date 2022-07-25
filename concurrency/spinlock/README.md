02d92d94c2f5c0918963a8c7f4501213a6915139
None <@none@none.com>
2022-07-25 10:32:55 +0300

    spinlock backoff

--- concurrency/spinlock/README.md
+++ concurrency/spinlock/README.md
@@ -4,3 +4,9 @@ Spin Lock using atomics and unsafe.
 
 - SpinLock
 - SpinLockGuard implements Deref DerefMut traits
+- Backoff
+
+## Todo
+
+- poisoning ?
+- tests and benches ?
--- concurrency/spinlock/src/lib.rs
+++ concurrency/spinlock/src/lib.rs
@@ -1,30 +1,31 @@
-use std::sync::atomic::{ AtomicBool, Ordering };
-use std::ops::{Deref, DerefMut};
-use std::cell::UnsafeCell;
-use std::thread;
 use core::hint;
+use std::{sync::atomic::{AtomicBool, Ordering}, ops::{Deref, DerefMut}, cell::UnsafeCell, thread};
 
-pub struct SpinLock<T> {
+pub struct SpinLock<T> { 
     atomic: AtomicBool,
     value: UnsafeCell<T>,
 }
 
-unsafe impl<T: Send> Send for SpinLock<T> {}
-unsafe impl<T: Send> Sync for SpinLock<T> {}
+pub struct SpinLockGuard<'a, T> {
+    lock: &'a SpinLock<T>,
+}
+
+struct Backoff { cnt: u32 }
 
 impl<T> SpinLock<T> {
     pub fn new(value: T) -> Self {
-        SpinLock { atomic: AtomicBool::new(true), value: UnsafeCell::new(value) }
+        SpinLock { atomic: AtomicBool::new(false), value: UnsafeCell::new(value) }
     }
 
     pub fn lock(&self) -> SpinLockGuard<T> {
-        self.inner_lock();
-        SpinLockGuard::new(self)
+        self.innner_lock();
+        SpinLockGuard { lock:  self }
     }
 
-    fn inner_lock(&self) {
-        while self.atomic.fetch_or(true, Ordering::Acquire) {
-            hint::spin_loop();
+    fn innner_lock(&self) {
+        let mut backoff = Backoff::new();
+        while self.atomic.swap(true, Ordering::AcqRel) {
+            backoff.backoff();
         }
     }
 
@@ -33,32 +34,45 @@ impl<T> SpinLock<T> {
     }
 }
 
-pub struct SpinLockGuard<'a, T> {
-    spin_lock: &'a SpinLock<T>,
-}
+unsafe impl<T: Send> Send for SpinLock<T> {}
 
-impl<'a, T> SpinLockGuard<'a, T> {
-    fn new(spin_lock: &'a SpinLock<T>) -> Self {
-        SpinLockGuard { spin_lock }
-    }
-}
+unsafe impl<T: Send> Sync for SpinLock<T> {}
 
-impl<'a, T>  Drop for SpinLockGuard<'a, T> {
+impl<'a, T> Drop for SpinLockGuard<'a, T> {
     fn drop(&mut self) {
-        self.spin_lock.inner_unlock();
+        self.lock.inner_unlock();
     }
 }
 
-impl<'a, T> Deref for SpinLockGuard<'a, T> {
+impl<'a, T>  Deref for SpinLockGuard<'a, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        unsafe { &*self.spin_lock.value.get() }
+        unsafe { &*self.lock.value.get() }
     }
 }
 
 impl<'a, T> DerefMut for SpinLockGuard<'a, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        unsafe { &mut *self.spin_lock.value.get() }
+        unsafe { &mut *self.lock.value.get() }
     }
+}
+
+impl Backoff {
+    const SPIN_LIMIT: u32 = 6;
+
+    fn new() -> Self {
+        Backoff { cnt: 0 }
+    }
+
+    fn backoff(&mut self) {
+        if self.cnt <= Self::SPIN_LIMIT {
+            for _ in 0..(1 << self.cnt) {
+                hint::spin_loop();
+            }
+            self.cnt += 1;
+        } else {
+            thread::yield_now();
+        }
+    }    
 }
